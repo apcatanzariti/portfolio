93f470c9807201bfa778ed96dfa269d580db215a
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-06-24 21:31:15 -0400

    created some pages to get started

---
+++ src/pages/HomePage/HomePageContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RenderHomePage () {
+    return (
+        <div>
+            <h1>This is the new home page component!</h1>
+        </div>
+    );
+}
+
+export default RenderHomePage;
